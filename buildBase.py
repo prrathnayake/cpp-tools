e8def59ffa8600029380a41184d7d2ab4c27ee3d
prrathnayake <pasanransika1@gmail.com>
2023-04-12 21:13:37 +0530

    add script to build all packages at once

---
+++ buildBase.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import re
+import subprocess
+
+if len(sys.argv) < 2 :
+    raise Exception("python3 " + sys.argv[0] + " path to ccp-base")
+ 
+rootdir = sys.argv[1]
+
+for file in os.listdir(rootdir):
+    dirs = os.path.join(rootdir, file)
+    if os.path.isdir(dirs):
+        if ("app" in dirs):
+            process = subprocess.Popen("conan create " + d + "/src", stdout=subprocess.PIPE, shell=True)
+            print(process)
+            process.wait()
