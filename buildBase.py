04ba8e73398a28c819043d5db2e9f8d818b06be9
prrathnayake <pasanransika1@gmail.com>
2023-04-13 01:01:32 +0530

    add functionalty to build only one packages

--- buildBase.py
+++ buildBase.py
@@ -2,16 +2,22 @@ import os
 import sys
 import re
 import subprocess
-
-if len(sys.argv) < 2 :
-    raise Exception("python3 " + sys.argv[0] + " path to ccp-base")
  
 rootdir = sys.argv[1]
 
 for file in os.listdir(rootdir):
     dirs = os.path.join(rootdir, file)
     if os.path.isdir(dirs):
-        if ("app" in dirs):
+        if (len(sys.argv) > 2) :
+            if(sys.argv[2] in dirs):
+                process = subprocess.Popen("conan create " + dirs + "/src", stdout=subprocess.PIPE, shell=True)
+                print(process)
+                process.wait()
+            
+        elif(len(sys.argv) == 2 and "app" in dirs):
             process = subprocess.Popen("conan create " + dirs + "/src", stdout=subprocess.PIPE, shell=True)
             print(process)
             process.wait()
+            
+        else:
+            print(("python3 " + sys.argv[0] + " path to ccp-base"))
